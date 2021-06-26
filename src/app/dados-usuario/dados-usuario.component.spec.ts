46f03726774f4f700fb4a65f19b9506d9ddc735b
Eriton Santos <dev.eritonsg@gmail.com>
2021-06-26 12:21:16 -0300

    Atualiza Angular para a versão 12

--- src/app/dados-usuario/dados-usuario.component.spec.ts
+++ src/app/dados-usuario/dados-usuario.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DadosUsuarioComponent } from './dados-usuario.component';
 
@@ -6,7 +6,7 @@ describe('DadosUsuarioComponent', () => {
   let component: DadosUsuarioComponent;
   let fixture: ComponentFixture<DadosUsuarioComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DadosUsuarioComponent ]
     })
