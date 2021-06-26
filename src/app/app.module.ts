5a06efadd11b96bc3f40b6b07c26ce956e41e769
Eriton Santos <dev.eritonsg@gmail.com>
2021-06-26 18:21:07 -0300

    Adiciona ngx-bootstrap e ngx-bootstrap-icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { checkCircleFill } from 'ngx-bootstrap-icons';
+
 import { AppComponent } from './app.component';
 import { DadosUsuarioComponent } from './dados-usuario/dados-usuario.component';
 import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 import { DetalhesUsuarioComponent } from './detalhes-usuario/detalhes-usuario.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 import { RodapeComponent } from './rodape/rodape.component';
+
+const icons = {
+  checkCircleFill
+};
 
 @NgModule({
   declarations: [
@@ -20,9 +28,14 @@ import { RodapeComponent } from './rodape/rodape.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AlertModule.forRoot(),
+    NgxBootstrapIconsModule.pick(icons)
   ],
   providers: [],
+  exports: [
+    AlertModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
